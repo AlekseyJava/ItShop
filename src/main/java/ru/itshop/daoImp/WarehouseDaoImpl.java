584bd9356c73f29cecade1190145610f0b93fc65
AlekseyJava <zvaraleksey@gmail.com>
2020-11-08 18:22:33 +0300

    work with persistents layers

---
+++ src/main/java/ru/itshop/daoImp/WarehouseDaoImpl.java
@@ -0,0 +1,22 @@
+package ru.itshop.daoImp;
+
+import ru.itshop.dao.WarehouseDao;
+import ru.itshop.database.DatabaseWarehouse;
+import ru.itshop.model.Product;
+
+public class WarehouseDaoImpl implements WarehouseDao {
+
+    DatabaseWarehouse databaseWarehouse = DatabaseWarehouse.getInstance();
+
+    @Override
+    public void addProduct_warehouse(Product product) {
+        databaseWarehouse.addProduct(product);
+        System.out.println("add product (ProductImp.class)");
+    }
+
+    @Override
+    public void deleteProduct_warehouse(Product product) {
+        databaseWarehouse.deleteProduct(product);
+        System.out.println("add product (ProductImp.class)");
+    }
+}
